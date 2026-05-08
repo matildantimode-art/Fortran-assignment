ead13f18692aee02576477970ea99e11a385a2ba
matildantimode-art <matildantimode@gmail.com>
2026-05-08 08:04:14 -0700

    Update matilda_6311224.f90

--- MET253_Assignment_repo/matilda_6311224.f90
+++ MET253_Assignment_repo/matilda_6311224.f90
@@ -1,28 +1,29 @@
 PROGRAM scores
 IMPLICIT NONE
+INTEGER,DIMENSION(10)::scores =(/85,62,45,91,38,74,55,88,61,47/)                           !Declaration of array to store 10 student scores
 INTEGER:: i
-READ*, i(1,10)
-a(1)=85
-a(2)=62
-a(3)=45
-a(4)=91
-a(5)=38
-a(6)=74
-a(7)=55
-a(8)=88
-a(9)=61
-a(10)=47
-PRINT*,i(1,10)
-  IF a=(80:100)
-  PRINT*, 'GRADE_(A)_distinction:'
-  END IF
-  ELSE IF a=(60:79), =(40:59), a=(0:39)
-  PRINT*, 'GRADE_(B)_Credit:','GRADE_(C)_pass:', 'GRADE_(D)_fail:'
-  END ELSE IF
-
-   DO i(1,10)
-   PRINT*, i(1,10)
-   END DO
+CHARACTER(LEN=1)::Grade                                                                     ! variable declaration
+CHARACTER(LEN=12)::Remark
+PRINT*,"..............................................."                                    !print table header
+PRINT*,"STUDENT       SCORE       GRADE       REMARK"
+PRINT*,"................................................"
+   DO i=1,10                                                                      !I added do loop for the code to run for 10 students
+    IF (scores(i) >=80  .AND.  scores(i) <=100)THEN                                !scre range is checked and grade and remark is assigned
+     Grade = 'A'
+     Remark = 'Distinction'
+    ELSE IF (scores(i) >=60  .AND.  scores(i) <=79)THEN
+     Grade = 'B'
+     Remark = 'Credit'
+    ELSE IF (scores(i) >=40  .AND.  scores(i) <=59)THEN
+     Grade = 'C'
+     Remark = 'Pass'
+    ELSE
+     Grade = 'F'
+     Remark = 'Fail'
+    END IF
+  WRITE(*,6) i, scores(i) , Grade , Remark                         !output results for each student
+  END DO
+ 6 FORMAT (I5,5X,I5,5X,A1,7X,A12)                                    !Format outputs columns for student number, scores, Grade, Remark      
 END PROGRAM scores
 !MATILDA NTIM
 !6311224
